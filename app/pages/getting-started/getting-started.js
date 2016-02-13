035b7cee525d83a09d31b363eeef411432e37fbc
upamune <jajkeqos@gmail.com>
2016-02-13 23:43:11 +0900

    :hatching_chick: init commit

---
+++ app/pages/getting-started/getting-started.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic/ionic';
+
+
+@Page({
+  templateUrl: 'build/pages/getting-started/getting-started.html'
+})
+export class GettingStartedPage {
+  constructor() {
+
+  }
+}
---
+++ app/pages/grid/grid.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic/ionic';
+
+
+@Page({
+  templateUrl: 'build/pages/grid/grid.html'
+})
+export class GridPage {
+  constructor(){
+
+  }
+}
